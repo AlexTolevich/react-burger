31fce0dc755a749b62b594c1af8463387c7007d1
Alexander Kuznetsov <alexanderanatolevic@yandex.ru>
2023-01-04 21:15:59 +0500

    fix: del unnecessary props

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,15 +5,12 @@ import BurgerConstructor from "../BurgerConstructor/BurgerConstructor";
 import style from "./Main.module.css";
 
 function Main() {
-  const [burger, setBurger] = React.useState([]);
-
   return (
     <>
       <AppHeader/>
       <main className={style.container}>
-        <BurgerIngredients
-          burger={burger} setBurger={setBurger}/>
-        <BurgerConstructor burger={burger}/>
+        <BurgerIngredients/>
+        <BurgerConstructor/>
       </main>
     </>
   )

5ac3f3c73676cf155954ea57b858c3cccb03c5b2
Alexander Kuznetsov <alexanderanatolevic@yandex.ru>
2023-03-02 18:03:41 +0500

    feat: add Feed page

--- src/pages/Feed/Feed.tsx
+++ src/pages/Feed/Feed.tsx
@@ -7,16 +7,96 @@ import {OrderItem} from "../../components/OrderItem/OrderItem";
 export const Feed: FC = () => {
 
   return (
-    <section>
-      <h2
-        className={'mt-10 mb-5 text text_type_main-large'}>
-        Лента заказов
-      </h2>
-      <ul className={`${style.list}`}>
-        {
-          testWsData.orders.map((order: IFeedOrderItem) => (
-          <OrderItem key={order._id} order={order}/>
-        ))}
-      </ul>
-    </section>)
+    <main className={style.feed}>
+      <section className={style.orderFeed}>
+        <h2
+          className={`mt-10 mb-5 text text_type_main-large`}>
+          Лента заказов
+        </h2>
+        <ul className={`${style.orderList}`}>
+          {
+            testWsData.orders.map((order: IFeedOrderItem) => (
+              <OrderItem key={order._id} order={order}/>
+            ))}
+        </ul>
+      </section>
+      <section className={`${style.statistics} pt-25 pb-10`}>
+        <div className={`${style.coverStatisticOrders}`}>
+          <div>
+            <p className={`text text_type_main-medium pb-6`}>
+              Готовы:
+            </p>
+            <div className={style.statusList}>
+              <ul className={`${style.orders}`}>
+                {testWsData.orders.filter((order) => order.status === 'done')
+                  .slice(0, 10)
+                  .map((order: IFeedOrderItem) => (
+                    <li className={`${style.order} ${style.orderStatusDone} text text_type_digits-default mb-2`}
+                        key={order._id}
+                    >
+                      {order.number}
+                    </li>
+                  ))}
+              </ul>
+              {testWsData.orders.length > 10 && (
+                <ul className={`${style.orders}`}>
+                  {testWsData.orders.filter((order) => order.status === 'done')
+                    .slice(11, 21)
+                    .map((order: IFeedOrderItem) => (
+                      <li className={`${style.order} ${style.orderStatusDone}  text text_type_digits-default mb-2`}
+                          key={order._id}
+                      >
+                        {order.number}
+                      </li>
+                    ))}
+                </ul>
+              )}
+            </div>
+          </div>
+          <div>
+            <p className={`text text_type_main-medium pb-6`}>
+              В работе:
+            </p>
+            <div className={style.statusList}>
+              <ul className={`${style.orders}`}>
+                {testWsData.orders.filter((order) => order.status === 'pending')
+                  .slice(0, 10)
+                  .map((order: IFeedOrderItem) => (
+                    <li className={`${style.order} text text_type_digits-default mb-2`}
+                        key={order._id}
+                    >
+                      {order.number}
+                    </li>
+                  ))}
+              </ul>
+              {testWsData.orders.length > 10 && (
+                <ul className={`${style.orders}`}>
+                  {testWsData.orders.filter((order) => order.status === 'pending')
+                    .slice(11, 21)
+                    .map((order: IFeedOrderItem) => (
+                      <li className={`${style.order} text text_type_digits-default mb-2`}
+                          key={order._id}
+                      >
+                        {order.number}
+                      </li>
+                    ))}
+                </ul>
+              )}
+            </div>
+          </div>
+        </div>
+        <p className={`text text_type_main-medium mt-15`}>
+          Выполнено за все время:
+        </p>
+        <p className={`${style.count} text text_type_digits-large`}>
+          {testWsData.total}
+        </p>
+        <p className={`text text_type_main-medium mt-15`}>
+          Выполнено за сегодня:
+        </p>
+        <p className={`${style.count} text text_type_digits-large`}>
+          {testWsData.totalToday}
+        </p>
+      </section>
+    </main>)
 }
